9732f0e4a0cc468557328fc35e0f1e57a6a0818d
tinagruber <s2210456010@fhooe.at>
2024-06-26 12:03:23 +0200

    added ganz viel

--- kochkompass/app/modal.tsx
+++ kochkompass/app/modal.tsx
@@ -7,9 +7,11 @@ import { Text, View } from '@/components/Themed';
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Modal</Text>
+      <Text style={styles.title}>We are CookingCompass</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/modal.tsx" />
+
+      <Text style={styles.container}>Who are we?</Text>
+
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
