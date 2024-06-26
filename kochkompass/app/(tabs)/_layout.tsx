f4ee2f41f6acdc099d2e3a4e992430293fbe1604
tinagruber <s2210456010@fhooe.at>
2024-06-26 11:23:00 +0200

    jetzt sind es drei Seiten

--- kochkompass/app/(tabs)/_layout.tsx
+++ kochkompass/app/(tabs)/_layout.tsx
@@ -54,6 +54,13 @@ export default function TabLayout() {
                     tabBarIcon: ({ color }) => <TabBarIcon name="hamburger" color={color} />,
                 }}
             />
+            <Tabs.Screen
+                name="karte"
+                options={{
+                    title: 'Shops',
+                    tabBarIcon: ({ color }) => <TabBarIcon name="location-arrow" color={color} />,
+                }}
+            />
         </Tabs>
     );
 }
