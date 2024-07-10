f2bbf930ca26eac3570ada17d1602949f3f3d11e
BernadetteR <radner.bern@gmail.com>
2024-07-10 14:07:02 +0200

    quickfix

--- kochkompass/components/RandomRecipe.tsx
+++ kochkompass/components/RandomRecipe.tsx
@@ -94,14 +94,6 @@ const styles = StyleSheet.create({
         paddingHorizontal: 10,
     },
     break: {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        height: 20, // Hier kannst du die gewünschte Höhe des Abstands anpassen
-=======
         height: 20,
->>>>>>> 41d8f36b3cb8336eb718ed033240dd5859d170fd
-=======
-        height: 20,
->>>>>>> 41d8f36b3cb8336eb718ed033240dd5859d170fd
     },
 });
